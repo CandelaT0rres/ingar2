17508dae51db590fe67a6e35160c01027ed4c103
CandelaT0rres <candetorretas@gmail.com>
2023-06-17 16:57:12 -0300

    cambios about as

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 
 export const About = () => {
@@ -6,63 +7,66 @@ export const About = () => {
   
   return (
     <div className='About ms-4 boton nav-link fw-bold fs-3'>
-      
-      <section class="row inicio-home-background section-1-home container-fluid">
-
     
-        <article className= "col-12 container ">
-                      
-          <div className="p-4 texto">
-            <h1 className="text-dark fw-bold text-center m-4">INGAR</h1>
-            <h3 className="text-lith fw-bold text-center">Sobre Nosotros</h3>
-          </div>
-
-        </article>
-
-
-      </section>
 
       <div className='contenedorDelSectorUno'>
 
       <div className='inicioUno-info'>
     
-        <p className='parrafo-inicio titulo-inicio'> Inicio</p>
+        <p className='parrafo-inicio p-1 titulo-inicio'>NUESTRO COMIENZO</p>
 
-        <p className='parrafo-inicio'> INGAR Consulting nace a principios del año 2015 fruto de observar y analizar las necesidades básicas del Mercado de Maquinaria Pesada y la industria dell Transporte</p>
+        <p className='parrafo-inicio p-1'> INGAR Consulting nace a principios del año 2015 fruto de observar y analizar las necesidades básicas del Mercado de Maquinaria Pesada y la industria dell Transporte</p>
 
-        <p className='parrafo-inicio titulo-inicio'> Mercado Actual</p>
+        <p className='parrafo-inicio titulo-inicio p-2'> MERCADO ACTUAL</p>
 
-        <p className='parrafo-inicio'>Encontramos un mercado con alta demanda de perfiles técnicos especializados en el mantenimiento de Maquinaria pesada y Vehículos Pesados.
+        <p className='parrafo-inicio p-2'>Encontramos un mercado con alta demanda de perfiles técnicos especializados en el mantenimiento de Maquinaria pesada y Vehículos Pesados.
         Este tipo de perfiles son escasos a nivel internacional.</p>
 
 
-        <p className='parrafo-inicio titulo-inicio'>Nuestras Ideas</p>
-        <p className='parrafo-inicio'>Desarrollamos nuestro porfatolio de productos y servicios en tres pilares que sirven de base para construír soluciones específicas y duraderas a las siguientes necesidades</p>
-        <p className='parrafo-inicio'>CAPACITACIÓN INTELIGENTE: En habiliades blandas, duras y específicas</p>
-        <p className='parrafo-inicio'> ASESORAMIENTO:Nuestros técnicos e ingenieros brindan soporte técnico en campo y soporte front office</p>
-        <p className='parrafo-inicio'>GERENCIAMIENTO: Focalizandonos en las necesidades de nuestros clientes y socios de negocios para las areas de mantenimiento y confiabilidad</p>
+        <p className='parrafo-inicio titulo-inicio p-3'>NUESTRAS IDEAS</p>
+        <p className='parrafo-inicio p-3'>Desarrollamos nuestro porfatolio de productos y servicios en tres pilares que sirven de base para construír soluciones específicas y duraderas a las siguientes necesidades</p>
+        <p className='parrafo-inicio p-3'>Capacitación Inteligente: En habiliades blandas, duras y específicas. Asesoramiento: Nuestros técnicos e ingenieros brindan soporte técnico en campo y soporte front office. Gerenciamiento: Focalizandonos en las necesidades de nuestros clientes y socios de negocios para las areas de mantenimiento y confiabilidad </p>
+     
               
       </div>
 
       <div className='inicioUno'>
-        <img src="./img/InicioUno.png"className='img-fluid w-90 h-auto'/>
+       
+
+        <div className='carruselDeAbout'>
+
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="img-fluid d-block w-90 imagenCarruselAboutUno"
+      src="./img/InicioUnoF.png"
+    />
+ 
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="img-fluid d-block w-90 imagenCarruselAboutDos"
+      src="./img/InicioDosF.png"
+    />
+
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="img-fluid d-block w-90 imagenCarruselAboutTres"
+      src="./img/InicioTresF.png"
+    />
+  </Carousel.Item>
+</Carousel>
+
+</div>
+
+
       </div>
    
       </div>
 
 
-       <section className="row inicio-home-background section-2-home container-fluid">
-          <article className= "col-12 container ">
-                        
-            <div className="p-4 texto2">
-              <h1 className="text-dark fw-bold text-center m-4 ingar">Nuestras Proyecciones</h1>
-              
-            </div>
-
-          </article>
-
-        </section>
-
+     
 
 
 
